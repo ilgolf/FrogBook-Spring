a8e3a8374cdab1c5cea67e701dc7132e40778055
ilgolf <user01@user01.com>
2021-09-30 01:46:06 +0900

    "OCP 개방 폐쇄 원칙"

---
+++ code/ocp/Matiz.java
@@ -0,0 +1,14 @@
+package ocp;
+
+public class Matiz  extends Car {
+
+    @Override
+    void open() {
+
+    }
+
+    @Override
+    void HandleGear() {
+
+    }
+}
---
+++ code/ocp2/Car.java
@@ -0,0 +1,7 @@
+package ocp2;
+
+public abstract class Car {
+    protected int energy; // 운전자 객체가 갖는 에너지
+    protected int gear;
+    protected boolean window; // true : 열림, false : 닫힘
+}
