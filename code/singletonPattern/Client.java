d95a553eb3a312f09b941d057db5cf07f9619972
ilgolf <user01@user01.com>
2021-10-07 11:26:27 +0900

    "디자인 패턴 Singleton 패턴"

---
+++ code/singletonPattern/Client.java
@@ -0,0 +1,21 @@
+package singletonPattern;
+
+public class Client {
+
+    public static void main(String[] args) {
+        // private 생성자이므로 new를 통해 인스턴스를 생성할 수 없다.
+        // Singleton s = new Singleton();
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        Singleton s3 = Singleton.getInstance();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+        s1 = null;
+        s2 = null;
+        s3 = null;
+    }
+}
